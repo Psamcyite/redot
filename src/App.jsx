1520fee1c5b37ccd50fe1c131477ef11a3eee323
Psamcyite <amojoayokunle@gmail.com>
2024-01-12 15:05:36 +0100

    create components folder which contains 'Header, Footer files'

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,25 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import axios from 'axios';
+import './App.css';
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name, setName] = useState();
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <Router>
+      <div className='app' style={{ backgroundImage: 'url("/ques1.png")'}}>
+        <Header />
+        <Routes>
+          <Route path='/' exact>
+            
+          </Route>
+        </Routes>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <div 
+        style={{
+            textAlign: 'center',
+            marginBottom: 10,
+        }}
+    >
+        Created by {' '}
+        <a
+            href='https://github.com/Psamcyite'
+        >
+            @Psamcyite
+        </a>
+    </div>
+  );
+};
+
+export default Footer;
