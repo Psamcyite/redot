beb679706dea8769a29b079f505cbbdeffbd1bfa
Psamcyite <amojoayokunle.com>
2024-01-17 02:43:31 -0600

    typo error fixed instead of lowercase

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,9 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
-import Home from "./Pages/Home/Home";
-import Quiz from "./Pages/Quiz/Quiz";
-import Result from "./Pages/Result/Result";
+import Home from "./pages/Home/Home";
+import Quiz from "./pages/Quiz/Quiz";
+import Result from "./pages/Result/Result";
 
 function App() {
   const [questions, setQuestions] = useState([]);
